069d33f6287a7c6d7fad14e9d0b157e3055e9547
Joud Alosaimi <joudalosaimi11@gmail,com>
2025-10-27 13:29:18 +0300

    Added a job detail with three methods GET,PUT,DELETE to get a single job, update it , and delete it

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import JobsIndex
+from .views import JobsIndex,JobDetail
 
 urlpatterns = [
-    path('jobs/', JobsIndex.as_view(), name='job_index')
+    path('jobs/', JobsIndex.as_view(), name='job_index'),
+    path('jobs/<int:job_id>/', JobDetail.as_view(), name='job_detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -17,7 +17,7 @@ class JobsIndex(APIView):
             # get all of the jobs from the DB
             queryset = Job.objects.all()
 
-            # Convert the DB object we got to JSON
+            # Convert the DB object we got to JSON using a serializer
             serializer = JobSerializer(queryset, many=True)
 
             return Response(serializer.data, status=status.HTTP_200_OK)
@@ -42,4 +42,54 @@ class JobsIndex(APIView):
             return Response(
                 {"error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
+        
+class JobDetail(APIView):
 
+    def get(self,request,job_id):
+        try:
+            # get a single job using the id fron the DB
+            queryset = get_object_or_404(Job, id=job_id)
+            # Convert the DB object to JSON
+            serializer = JobSerializer(queryset)
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as error:
+            return Response(
+                {"error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+    
+    def put(self,request,job_id):
+        try:
+            # get a single job using the id fron the DB
+            queryset = get_object_or_404(Job, id=job_id)
+
+            # Overwrite the single job with the new data using request.data
+            serializer = JobSerializer(queryset, data=request.data)
+            
+            if serializer.is_valid():
+                serializer.save()  # save it if its valid
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as error:
+            return Response(
+                {"error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+    
+    def delete(self,request,job_id):
+        try:
+            # Get the single job from the db
+            queryset = get_object_or_404(Job, id=job_id)
+
+            # Delete the job
+            queryset.delete()
+
+            return Response(
+                {"message": f"The job {job_id} has been successfully deleted."},
+                status=status.HTTP_204_NO_CONTENT
+            )
+
+        except Exception as error:
+            return Response(
+                {"error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
