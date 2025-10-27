4e44458b202ff6884811b11e2dceb0d70ed0d8bb
Joud Alosaimi <joudalosaimi11@gmail,com>
2025-10-27 23:54:33 +0300

    added a path for the BooutcampsIndex and tested it using postman

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import JobsIndex,JobDetail,CoursesIndex,CourseDetail
+from .views import JobsIndex,JobDetail,CoursesIndex,CourseDetail,BootcampsIndex
 
 urlpatterns = [
     path('jobs/', JobsIndex.as_view(), name='job_index'),
     path('jobs/<int:job_id>/', JobDetail.as_view(), name='job_detail'),
     path('courses/', CoursesIndex.as_view(), name='course_index'),
-    path('courses/<int:course_id>/', CourseDetail.as_view(), name='course_detail')
+    path('courses/<int:course_id>/', CourseDetail.as_view(), name='course_detail'),
+    path('bootcamps/', BootcampsIndex.as_view(), name='bootcamp_index')
 ]
