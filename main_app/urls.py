bb003ba162ced1ae2b85a6986b6c80926c90f381
Joud Alosaimi <joudalosaimi11@gmail,com>
2025-10-28 13:16:26 +0300

    created a new path for the ApplicationDetail and tested it using postman

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import JobsIndex,JobDetail,CoursesIndex,CourseDetail,BootcampsIndex,BootcampDetail,ApplicationIndex
+from .views import JobsIndex,JobDetail,CoursesIndex,CourseDetail,BootcampsIndex,BootcampDetail,ApplicationIndex,ApplicationDetail
 
 urlpatterns = [
     path('jobs/', JobsIndex.as_view(), name='job_index'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('courses/<int:course_id>/', CourseDetail.as_view(), name='course_detail'),
     path('bootcamps/', BootcampsIndex.as_view(), name='bootcamp_index'),
     path('bootcamps/<int:bootcamp_id>/', BootcampDetail.as_view(), name='bootcamp_detail'),
-    path('applications/', ApplicationIndex.as_view(), name='application_index')
+    path('applications/', ApplicationIndex.as_view(), name='application_index'),
+    path('applications/<int:application_id>/', ApplicationDetail.as_view(), name='application_detail')
 ]
