d8b0c298a7bdadb42071422125338f5b55daa8c3
Joud Alosaimi <joudalosaimi11@gmail,com>
2025-10-28 13:02:42 +0300

    added a PUT method to the ApplicationDetail

--- main_app/views.py
+++ main_app/views.py
@@ -341,3 +341,23 @@ class ApplicationDetail(APIView):
             return Response(
                 {"error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
+    
+    def put(self,request,application_id):
+        try:
+            # TODO :
+            # get a single application
+            # Overwrite the application
+            # save it
+            queryset = get_object_or_404(Application, id=application_id)
+
+            serializer = ApplicationSerializer(queryset, data=request.data)
+            
+            if serializer.is_valid():
+                serializer.save()  
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as error:
+            return Response(
+                {"error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
